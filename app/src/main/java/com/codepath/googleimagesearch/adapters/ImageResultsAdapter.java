0087adbb83c5cd8f2aec6c1c22910711c06be46e
kprav <praveen.86@gmail.com>
2015-09-27 13:14:26 -0700

    ViewHolderPattern

--- app/src/main/java/com/codepath/googleimagesearch/adapters/ImageResultsAdapter.java
+++ app/src/main/java/com/codepath/googleimagesearch/adapters/ImageResultsAdapter.java
@@ -17,24 +17,44 @@ import java.util.List;
 
 public class ImageResultsAdapter extends ArrayAdapter<ImageResult> {
 
+    // View lookup cache for the view holder pattern
+    private static class ViewHolder {
+        ImageView ivImage;
+        TextView tvTitle;
+    }
+
     public ImageResultsAdapter(Context context, List<ImageResult> images) {
         super(context, R.layout.item_image_result, images);
     }
 
+    // Get the data item and display it using the layout XML
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data for this position
         ImageResult imageInfo = getItem(position);
+
+        // Cache Object
+        ViewHolder viewHolder;
+
         if (convertView == null) {
+            // Inflate a new view
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_image_result, parent, false);
+
+            // Create a new viewHolder cache
+            viewHolder = new ViewHolder();
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        // Clear out image from last time
-        ivImage.setImageResource(0);
-        // Populate title and remote download image url
-        tvTitle.setText(Html.fromHtml(imageInfo.getTitle()));
-        // Remotely download the image data in the background (with Picasso)
-        Picasso.with(getContext()).load(imageInfo.getThumbUrl()).into(ivImage);
+
+        // Set the data to the viewHolder
+        viewHolder.ivImage.setImageResource(0);
+        Picasso.with(getContext()).load(imageInfo.getThumbUrl()).into(viewHolder.ivImage);
+        viewHolder.tvTitle.setText(Html.fromHtml(imageInfo.getTitle()));
+
         // Return the completed view to be displayed
         return convertView;
     }
